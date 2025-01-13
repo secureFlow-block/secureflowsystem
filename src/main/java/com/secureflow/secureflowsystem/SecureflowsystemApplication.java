56170e81fd1a2beca761da35df6a6ff5c331b9c2
elanecoelho <ellianee@hormail.com>
2025-01-13 15:06:23 -0300

    first commit

---
+++ src/main/java/com/secureflow/secureflowsystem/SecureflowsystemApplication.java
@@ -0,0 +1,13 @@
+package com.secureflow.secureflowsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureflowsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureflowsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secureflow/secureflowsystem/SecureflowsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureflow.secureflowsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureflowsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
